124006e058b42f9cde41794152daee57cbf3c69f
Abdulhakeem Shaibu <hkmshb@gmail.com>
2019-07-20 13:30:00 +0100

    refactor custom server for nextjs as VersityServer

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,44 @@
-import express, { Express } from "express";
 import next from "next";
+import express, { Express } from "express";
+import { CLIENT_PATH } from "./constants";
 
 
-const dev: boolean = process.env.NODE_ENV !== 'production';
-const app = next({ dir: './src/client', dev });
+class VersityServer {
+  public app: Express;
+  public clientApp: any;  // TODO: use proper type
+
+  constructor(clientPath: string, debug: boolean=true) {
+    this.app = express();
+    this.clientApp = next({ dir: clientPath, dev: debug });
+    this.configureApp();
+  }
+
+  private configureApp(): void {
+    this.configureClientServer();
+    this.configureApiServer();
+  }
+
+  private configureClientServer(): void {
+    // handle all non-API related requests
+    const handler = this.clientApp.getRequestHandler();
+    this.app.get('*', (req, res) => handler(req, res));
+  }
+
+  private configureApiServer() {
+    // TODO: pending...
+  }
+
+  public ready(callback: (app: Express) => void): void {
+    this.clientApp.prepare().then(() => {
+      callback(this.app);
+    });
+  }
+}
 
 const port: number = Number(process.env.PORT) || 3000;
-const handler = app.getRequestHandler();
+const debug: boolean = process.env.NODE_ENV !== 'production';
 
-
-app.prepare().then(() => {
-  const server: Express = express();
-
-  server.get('*', (req, res) => handler(req, res));
-  server.listen(port, () => console.log(`>> versity up and running @ localhost:${port}`))
+const server = new VersityServer(CLIENT_PATH, debug);
+server.ready((app: Express) => {
+  app.listen(port, () => console.log(`\n>> Versity running at "http://localhost:${port}/"`));
 });
